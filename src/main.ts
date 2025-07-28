241ac36ecea011fcab59c39bfeca898514d13264
VHall1 <victorhallpsn@hotmail.com>
2025-07-29 00:48:48 +0100

    remove pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import { createPinia } from "pinia";
 import { createApp } from "vue";
 import "./tailwind.css";
 
 import App from "./App.vue";
 
 const app = createApp(App);
-
-app.use(createPinia());
 
 app.mount("#app");
