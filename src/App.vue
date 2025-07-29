98dc1e7d0e4dc63fff75f6f1d7668e1c40d1a24f
VHall1 <victorhallpsn@hotmail.com>
2025-07-29 01:25:58 +0100

    add basic styles

--- src/App.vue
+++ src/App.vue
@@ -23,19 +23,31 @@ const handleKeydown = (payload: KeyboardEvent) => {
 };
 </script>
 <template>
-  <h1 class="text-3xl font-bold">todos</h1>
-  <div>
-    <label for="new-todo" class="sr-only">Create a new todo</label>
-    <input
-      id="new-todo"
-      v-model="newTodo"
-      placeholder="What needs to be done?"
-      @keydown="handleKeydown"
-    />
-  </div>
+  <div class="max-w-xl mx-auto">
+    <h1 class="text-7xl font-extralight text-center text-red-700 mt-10">todos</h1>
 
-  <div v-for="todo in todos" :key="todo.id">
-    <input type="checkbox" v-model="todo.done" />
-    <label>{{ todo.description }}</label>
+    <div class="mt-5">
+      <label for="new-todo" class="sr-only">Create a new todo</label>
+      <input
+        class="p-4 w-full shadow-lg outline-1 placeholder:italic placeholder:text-2xl"
+        id="new-todo"
+        v-model="newTodo"
+        placeholder="What needs to be done?"
+        @keydown="handleKeydown"
+      />
+    </div>
+
+    <div class="mt-5">
+      <div v-for="todo in todos" :key="todo.id" class="py-4">
+        <label
+          :for="todo.id"
+          class="text-2xl flex items-center"
+          :class="{ 'line-through': todo.done }"
+        >
+          <input :id="todo.id" class="size-10 mr-5" type="checkbox" v-model="todo.done" />
+          {{ todo.description }}
+        </label>
+      </div>
+    </div>
   </div>
 </template>
