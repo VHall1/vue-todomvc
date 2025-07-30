5a16e3541005a0429cba84ef04c18201270f0df1
vhall1 <victorhallpsn@hotmail.com>
2025-07-30 21:02:55 +0100

    add pinia for state management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import "./tailwind.css";
 
 import App from "./App.vue";
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.mount("#app");
